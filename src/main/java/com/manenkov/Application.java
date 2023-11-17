343ae960e7d271d454c500df9e4683395f0c4920
Valeriy Manenkov <valeriy@manenkov.com>
2023-11-17 14:36:06 +0300

    add source code

---
+++ src/main/java/com/manenkov/Application.java
@@ -0,0 +1,8 @@
+package com.manenkov;
+
+public class Application {
+    public static void main(final String[] args) {
+        System.out.println("It's works!");
+    }
+}
+
---
+++ src/test/java/com/manenkov/ApplicationTest.java
@@ -0,0 +1,7 @@
+package com.manenkov;
+
+public class ApplicationTest {
+    public static void main(final String[] args) {
+        System.out.println("ApplicationTest");
+    }
+}

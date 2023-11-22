7a6221c55285f4d06c86cb56ac9963a2badf65fa
Valeriy Manenkov <valeriy@manenkov.com>
2023-11-22 23:24:33 +0300

    change licenses

--- src/main/java/com/manenkov/Application.java
+++ src/main/java/com/manenkov/Application.java
@@ -1,24 +1,11 @@
 package com.manenkov;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 public class Application {
     public static void main(final String[] args) throws Exception {
-        try {
-            new Application().execute();
-            System.exit(0);
-        } catch (final Exception exception) {
-            exception.printStackTrace();
-            System.exit(1);
-        }
+        new Application().execute();
     }
 
     void execute() throws Exception {
-        try (final InputStream inputStream = getClass().getResourceAsStream("/application.properties")) {
-            final Properties properties = new Properties();
-            properties.load(inputStream);
-            System.out.println(properties.getProperty("hello"));
-        }
+        System.out.println("It's works!");
     }
 }
